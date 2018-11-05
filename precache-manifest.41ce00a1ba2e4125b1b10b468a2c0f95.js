9551f063a92750c16170ec3a9ae972ac9791e78d
pjchender <pjchender@gmail.com>
2018-11-05 17:52:16 +0800

    Updates

---
+++ precache-manifest.41ce00a1ba2e4125b1b10b468a2c0f95.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "061733c886729d5f5795",
+    "url": "/static/js/main.061733c8.chunk.js"
+  },
+  {
+    "revision": "8d28052f94d24220fd95",
+    "url": "/static/js/1.8d28052f.chunk.js"
+  },
+  {
+    "revision": "061733c886729d5f5795",
+    "url": "/static/css/main.dc9f7dbe.chunk.css"
+  },
+  {
+    "revision": "b266c684c3306e6c23f358fe1fa3f994",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/wavinfo-hr-pwa/precache-manifest.8985fd1d98083cbcb459490604e9cebb.js"
+  "/precache-manifest.41ce00a1ba2e4125b1b10b468a2c0f95.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/wavinfo-hr-pwa/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
